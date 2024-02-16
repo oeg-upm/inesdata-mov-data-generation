883cc6350a3fd03491f5013f3c6c513aa0adc5a1
Javier Fontecha Guadaño <jfontecha@gmv.com>
2024-02-16 13:03:19 +0100

    Added lines to config file

--- inesdata_mov_datasets/settings.py
+++ inesdata_mov_datasets/settings.py
@@ -38,6 +38,7 @@ class SourceEmtCredentialsSettings(BaseModel):
 class SourceEmtSettings(BaseModel):
     credentials: SourceEmtCredentialsSettings
     stops: List[int]
+    lines: List[int]
 
 
 class SourceAemetCredentialsSettings(BaseModel):
