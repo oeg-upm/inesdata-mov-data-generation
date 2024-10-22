888f2aef5540e52685df45b4094a0d24284e07fb
mlia <mlia@ltmlia.gmv.es>
2024-10-22 16:38:25 +0200

    trying to remove asynchronisation from the get_filter_emt function

--- inesdata_mov_datasets/sources/extract_filtered/emt.py
+++ inesdata_mov_datasets/sources/extract_filtered/emt.py
@@ -5,6 +5,8 @@ import datetime
 import json
 import traceback
 
+import requests
+
 import aiohttp
 import pytz
 from loguru import logger
@@ -48,49 +50,131 @@ async def get_eta(session: aiohttp, stop_id: str, line_id: str, headers: json) -
             logger.error(e)
             return {"code": -1}
 
-async def get_filter_emt_2(config: Settings, stop_id: str, line_id: str):
-    """Get all the data from EMT endpoints.
+def get_eta_sync(stop_id: str, line_id: str, headers: json) -> json:
+    """Make the API call to ETA endpoint.
 
     Args:
-        config (Settings): Object with the config file.
-        stop_id (str): The stop id.
-        line_id (str): The line id.
+        session (aiohttp): Call session to make faster the calls to the same API.
+        stop_id (str): Id of the bus stop.
+        line_id (str): Id of the bus line.
+        headers (json): Headers of the http call.
+
+    Returns:
+        json: Response of the petition in json format.
     """
+    body = {
+        "cultureInfo": "ES",
+        "Text_StopRequired_YN": "N",
+        "Text_EstimationsRequired_YN": "Y",
+        "Text_IncidencesRequired_YN": "N",
+    }
+    
+    eta_url = (
+        f"https://openapi.emtmadrid.es/v2/transport/busemtmad/stops/{stop_id}/arrives/{line_id}"
+    )
+
     try:
-        st = datetime.datetime.now()
-        # Get the timezone from Madrid and format the dates for the object_name of the files
-        europe_timezone = pytz.timezone("Europe/Madrid")
-        current_datetime = datetime.datetime.now(europe_timezone).replace(second=0)
-        formatted_date_day = current_datetime.strftime("%Y%m%d")
-        formatted_date_slash = current_datetime.strftime("%Y/%m/%d")
-
-        # Obtain token from EMT
-        access_token = await token_control(config, formatted_date_slash, formatted_date_day)
-
-        headers = {
-            "accessToken": access_token,
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-        }
-
-        async with aiohttp.ClientSession() as session:
-            # Perform both requests concurrently using asyncio.gather
-            eta_response, calendar_response = await asyncio.gather(
-                get_eta(session, stop_id, line_id, headers),
-                get_calendar(session, formatted_date_day, formatted_date_day, headers)
-            )
-
-            logger.info("Extracted EMT")
-            et = datetime.datetime.now()
-            print('second test', (et - st).total_seconds())
-            return eta_response, calendar_response
-
+        response = requests.post(eta_url, headers=headers, json=body)
+        response.raise_for_status()
+        return response.json()
     except Exception as e:
+        logger.error(f"Error in ETA call stop {stop_id} to the server")
         logger.error(e)
-        logger.error(traceback.format_exc())
+        return {"code": -1}
+    
+def get_calendar_sync(start_date: str, end_date: str, headers: dict) -> dict:
+    """Call Calendar endpoint EMT.
 
+    Args:
+        start_date (str): Start date of the date you want to check.
+        end_date (str): End date of the date you want to check.
+        headers (dict): Headers of the HTTP petition.
 
-async def get_filter_emt_1(config: Settings, stop_id: str, line_id: str):
+    Returns:
+        dict: Response of the petition in JSON format.
+    """
+    calendar_url = f"https://openapi.emtmadrid.es/v1/transport/busemtmad/calendar/{start_date}/{end_date}/"
+    
+    try:
+        # Realiza la llamada GET y obtiene la respuesta
+        response = requests.get(calendar_url, headers=headers)
+        response.raise_for_status()  # Lanza una excepción si la respuesta es un error
+        return response.json()  # Devuelve la respuesta en formato JSON
+    except Exception as e:
+        logger.error("Error in calendar call to the server")
+        logger.error(e)
+        return {"code": -1}  # Devuelve un código de error en caso de fallo
+
+from pathlib import Path
+import os
+from inesdata_mov_datasets.utils import check_local_file_exists
+def login_emt_sync(config: Settings, object_login_name: str, local_path: Path ):
+    if config.sources.emt.credentials.x_client_id is not None:
+        headers = {
+            "X-ClientId": config.sources.emt.credentials.x_client_id,
+            "passKey": config.sources.emt.credentials.passkey,
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }
+    else:
+        headers = {
+            "email": config.sources.emt.credentials.email,
+            "password": config.sources.emt.credentials.password,
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }
+    r = requests.get(
+        "https://openapi.emtmadrid.es/v2/mobilitylabs/user/login/", headers=headers, verify=True
+    )
+    try:
+        login_json = r.json()
+        login_json_str = json.dumps(login_json)
+        token = login_json["data"][0]["accessToken"]
+        os.makedirs(local_path, exist_ok=True)
+        with open(os.path.join(local_path, object_login_name), "w") as file:
+            file.write(login_json_str)
+
+        return token
+    except Exception as e:
+        logger.error("Error in login call to the server")
+        logger.error(e)
+        return ""
+    
+def token_control_sync(config: Settings, date_slash: str, date_day: str):
+    dir_path = Path(config.storage.config.local.path) / "raw" / "emt" / date_slash / "login"
+    object_login_name = f"login_{date_day}.json"
+
+    if not check_local_file_exists(dir_path, object_login_name):
+        token = login_emt_sync(config, object_login_name, local_path=dir_path)
+        return token
+    # If it exists, get the token from the json
+    else:
+        with open(os.path.join(dir_path, object_login_name), "r") as file:
+            response = file.read()
+            data = json.loads(response)
+            token = data["data"][0]["accessToken"]
+            
+            #Catching an error that ocurrs when the server doesnt provide the token expiration
+            try:
+                expiration_date_unix = data["data"][0]["tokenDteExpiration"]["$date"]
+            except:
+                logger.error(f"Error saving time expiration from login. Solving the problem retrying the call.")
+                token = login_emt_sync(config, object_login_name, local_path=dir_path)
+                return token
+            
+            expiration_date = datetime.datetime.utcfromtimestamp(
+                expiration_date_unix / 1000
+            )  #  miliseconds to seconds
+            now = datetime.datetime.now()
+            # Compare the time expiration of the token withthe actual date
+            if now >= expiration_date:  # reset token
+                token = login_emt_sync(config, object_login_name, local_path=dir_path)
+                return token
+            # Get the token that already exists
+            elif now < expiration_date:
+                return token
+
+def get_filter_emt_sync(config: Settings, stop_id: str, line_id: str):
     """Get all the data from EMT endpoints.
 
     Args:
@@ -99,7 +183,7 @@ async def get_filter_emt_1(config: Settings, stop_id: str, line_id: str):
         line_id (str): The line id.
     """
     try:
-        st = datetime.datetime.now()
+        # st = datetime.datetime.now()
         # Get the timezone from Madrid and format the dates for the object_name of the files
         europe_timezone = pytz.timezone("Europe/Madrid")
         current_datetime = datetime.datetime.now(europe_timezone).replace(second=0)
@@ -107,25 +191,22 @@ async def get_filter_emt_1(config: Settings, stop_id: str, line_id: str):
         formatted_date_slash = current_datetime.strftime("%Y/%m/%d")
 
         # Obtain token from EMT
-        access_token = await token_control(config, formatted_date_slash, formatted_date_day)
+        access_token =token_control_sync(config, formatted_date_slash, formatted_date_day)
 
         headers = {
             "accessToken": access_token,
             "Content-Type": "application/json",
             "Accept": "application/json",
         }
+        # Realizar ambas solicitudes de forma sincrónica
+        eta_response = get_eta_sync(stop_id, line_id, headers)  # Reemplazar con una función sincrónica
+        calendar_response = get_calendar_sync(formatted_date_day, formatted_date_day, headers)  # Reemplazar con una función sincrónica
+        
 
-        async with aiohttp.ClientSession() as session:
-            # Perform both requests concurrently using asyncio.gather
-            eta_task = get_eta(session, stop_id, line_id, headers)
-            calendar_task = get_calendar(session, formatted_date_day, formatted_date_day, headers)
-
-            eta_response, calendar_response = await asyncio.gather(eta_task, calendar_task)
-
-            logger.info("Extracted EMT")
-            et = datetime.datetime.now()
-            print('first test',(et-st).total_seconds())
-            return eta_response, calendar_response
+        logger.info("Extracted EMT")
+        # et = datetime.datetime.now()
+        # print('sincrono',(et-st).total_seconds())
+        return eta_response, calendar_response
 
     except Exception as e:
         logger.error(e)
@@ -140,7 +221,7 @@ async def get_filter_emt(config: Settings, stop_id: str, line_id: str):
         line_id (str): The line id.
     """
     try:
-        st = datetime.datetime.now()
+        # st = datetime.datetime.now()
         # Get the timezone from Madrid and formated the dates for the object_name of the files
         europe_timezone = pytz.timezone("Europe/Madrid")
         current_datetime = datetime.datetime.now(europe_timezone).replace(second=0)
@@ -183,13 +264,18 @@ async def get_filter_emt(config: Settings, stop_id: str, line_id: str):
 
             logger.info("Extracted EMT")
 
-            et = datetime.datetime.now()
-            print('ORIGINAL',(et-st).total_seconds())
+            # et = datetime.datetime.now()
+            # print('asincrono',(et-st).total_seconds())
             return eta_responses[0], calendar_responses[0]
 
     except Exception as e:
         logger.error(e)
         logger.error(traceback.format_exc())
 
+# if __name__ == "__main__":
+#     sett = read_settings('/home/mlia/proyectos/data-generation/config_dev.yaml')
+#     # asyncio.run(get_filter_emt_sync(config=sett, stop_id='29', line_id='27'))
+#     print('asi ----->',asyncio.run(get_filter_emt(config=sett, stop_id='29', line_id='27')))
+#     print('sy ----->',get_filter_emt_sync(config=sett, stop_id='29', line_id='27'))
 
 
